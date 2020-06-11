2278ee598f2cacf68db7bde32a8c289ef785eddc
Lucie Drahonovska <luckadrahonovska@seznam.cz>
2020-06-11 17:00:47 +0200

    board preparation

--- src/Board.hs
+++ src/Board.hs
@@ -6,7 +6,7 @@ import System.Random
 
 data Status = Covered | Discovered | Flagged
 
-data State = Mine | Neighbour Int
+data State = Mine | Neighbour Int deriving (Eq, Show)
 
 type Board = Array (Char, Int) (State, Status)
 
@@ -26,3 +26,24 @@ genBoard ch x n = do
   m <- genMines ch x n
   let initBoard = array (('a', 1), (ch, x)) [((i, j), (Neighbour 0, Covered)) | i <- ['a' .. ch], j <- [1 .. x]]
   return $ initBoard // [((i, j), (Mine, Covered)) | i <- ['a' .. ch], j <- [1 .. x], (i, j) `elem` m]
+
+evalBoard :: Board -> Board
+evalBoard b = listArray (bounds b) (map (\a -> (a, Covered)) listCounts)
+  where
+    listCounts = map (_ . evalSpot b) (indices b)
+
+evalSpot :: Board -> (Char, Int) -> Int
+evalSpot b (ch, i) = length $ filter (== Mine) stateList
+  where
+    stateList = map (fst . (\a -> b ! a)) coords
+    coords = filter (\a -> a `elem` indices b) initList
+    initList =
+      [ (succ ch, i),
+        (pred ch, i),
+        (succ ch, i + 1),
+        (pred ch, i + 1),
+        (succ ch, i - 1),
+        (pred ch, i - 1),
+        (ch, i + 1),
+        (ch, i - 1)
+      ]
